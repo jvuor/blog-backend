0024050a9049dfd904c2aa6418a8ed8f7aeeb936
jvuor <jussi.vuorinen@gmail.com>
2018-07-23 22:34:21 +0300

    disables cors for same-origin calls

--- utils/cors.js
+++ utils/cors.js
@@ -2,9 +2,10 @@ var whitelist = ['http://localhost:3000']
 
 var corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
+      console.log(origin)
       callback('Not allowed by CORS')
     }
   }
