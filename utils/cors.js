9dd5a0a4d76a5c1fca19914d137ba23bb910f0b9
jvuor <jussi.vuorinen@gmail.com>
2018-11-03 16:09:51 +0200

    cors change for the new domain

--- utils/cors.js
+++ utils/cors.js
@@ -1,4 +1,4 @@
-var whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3003', 'https://jvuor-blog.herokuapp.com']
+var whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3003', 'https://jvuor-blog.herokuapp.com', 'http://localhost:4200', 'https://www.jussi.app']
 
 var corsOptions = {
   origin: function (origin, callback) {
