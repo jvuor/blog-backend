fb91e6749b5c18197e650ee680972cfd3d8ea0ca
Jussi Vuorinen <jussi.vuorinen@gmail.com>
2021-03-28 19:22:37 +0300

    remove built frontend apps from the repository

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(middleware.error)
 
 const server = http.createServer(app)
 
-server.listen(port, () => {
+server.listen(port,'0.0.0.0', () => {
   console.log(`Server running on port ${port}`)
 })
 
